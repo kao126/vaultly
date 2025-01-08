89da601d5aa5ec71fb54a8def6673f8ac9a1fba7
kao126 <76607249+kao126@users.noreply.github.com>
2025-01-08 22:25:27 +0900

    update: display selected images on top page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-// import 'dart:io';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mime/mime.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -58,14 +60,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<XFile>? _mediaFileList;
+  // int _counter = 0;
 
   // File? image;
+  dynamic _pickImageError;
+  bool isVideo = false;
+  String? _retrieveDataError;
+  final ImagePicker _picker = ImagePicker();
   // 画像をギャラリーから選ぶ関数
   Future pickImage() async {
     try {
-      final ImagePicker picker = ImagePicker();
-      final List<XFile> medias = await picker.pickMultipleMedia();
+      final List<XFile> pickedFileList = await _picker.pickMultipleMedia();
       // final List<XFile>? images = await picker.pickMultiImage();
       //   final image = await ImagePicker().pickImage(source: ImageSource.gallery);
       //   // 画像がnullの場合戻る
@@ -74,8 +80,101 @@ class _MyHomePageState extends State<MyHomePage> {
       //   final imageTemp = File(image.path);
 
       //   setState(() => this.image = imageTemp);
-    } on PlatformException catch (e) {
-      print('Failed to pick images/videos: $e');
+      setState(() {
+        _mediaFileList = pickedFileList;
+      });
+    } catch (e) {
+      setState(() {
+        _pickImageError = e;
+      });
+    }
+    // } on PlatformException catch (e) {
+    //   print('Failed to pick images/videos: $e');
+    // }
+  }
+
+  Widget _previewVideo() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    // if (_controller == null) {
+    return const Text(
+      'You have not yet picked a video',
+      textAlign: TextAlign.center,
+    );
+    // }
+    // return Padding(
+    //   padding: const EdgeInsets.all(10.0),
+    //   child: AspectRatioVideo(_controller),
+    // );
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_mediaFileList != null) {
+      // return Semantics(
+      //   label: 'image_picker_example_picked_images',
+      //   child: ListView.builder(
+      //     key: UniqueKey(),
+      //     itemBuilder: (BuildContext context, int index) {
+      //       final String? mime = lookupMimeType(_mediaFileList![index].path);
+
+      //       // Why network for web?
+      //       // See https://pub.dev/packages/image_picker_for_web#limitations-on-the-web-platform
+      //       return Semantics(
+      //         label: 'image_picker_example_picked_image',
+      //         child: kIsWeb
+      //             ? Image.network(_mediaFileList![index].path)
+      //             : (mime == null || mime.startsWith('image/')
+      //                 ? Image.file(
+      //                     File(_mediaFileList![index].path),
+      //                     errorBuilder: (BuildContext context, Object error,
+      //                         StackTrace? stackTrace) {
+      //                       return const Center(
+      //                           child:
+      //                               Text('This image type is not supported'));
+      //                     },
+      //                   )
+      //                 : _buildInlineVideoPlayer(index)),
+      //       );
+      //     },
+      //     itemCount: _mediaFileList!.length,
+      //   ),
+      // );
+      return ListView.builder(
+          key: UniqueKey(),
+          itemCount: _mediaFileList!.length,
+          itemBuilder: (BuildContext context, int index) {
+            final String? mime = lookupMimeType(_mediaFileList![index].path);
+            return (mime == null || mime.startsWith('image/'))
+                ? SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.file(File(_mediaFileList![index].path)))
+                : null;
+          });
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Widget _handlePreview() {
+    if (isVideo) {
+      return _previewVideo();
+    } else {
+      return _previewImages();
     }
   }
 
@@ -109,34 +208,34 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          // child: Column(
+          //   // Column is also a layout widget. It takes a list of children and
+          //   // arranges them vertically. By default, it sizes itself to fit its
+          //   // children horizontally, and tries to be as tall as its parent.
+          //   //
+          //   // Column has various properties to control how it sizes itself and
+          //   // how it positions its children. Here we use mainAxisAlignment to
+          //   // center the children vertically; the main axis here is the vertical
+          //   // axis because Columns are vertical (the cross axis would be
+          //   // horizontal).
+          //   //
+          //   // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          //   // action in the IDE, or press "p" in the console), to see the
+          //   // wireframe for each widget.
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: <Widget>[
+          //     const Text(
+          //       'You have pushed the button this many times:',
+          //     ),
+          //     Text(
+          //       '$_counter',
+          //       style: Theme.of(context).textTheme.headlineMedium,
+          //     ),
+          //   ],
+          // ),
+          child: _handlePreview()),
       floatingActionButton: FloatingActionButton(
         // onPressed: _incrementCounter,
         onPressed: pickImage,
@@ -144,5 +243,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
   }
 }
